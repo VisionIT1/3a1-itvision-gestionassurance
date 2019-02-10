b98ac9a8fba76b3c899851056b761dda06f5d0d6
youssefabid <youssef.abid@esprit.tn>
2019-02-10 17:55:00 +0100

    JavaFx

---
+++ GestionAssuranceFx/src/gestionassurancefx/Entities/Sinistre.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestionassurancefx.Entities;
+
+/**
+ *
+ * @author youssef
+ */
+public class Sinistre {
+    private String Code_sinistre;
+    private String Date_declaration;
+    private String Lieu_sinistre ;
+    
+    
+}
