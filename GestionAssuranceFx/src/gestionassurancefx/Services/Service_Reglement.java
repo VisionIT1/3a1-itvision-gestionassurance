0ba95788b30f094c6313696a1b921c5067ea67c8
youssefabid <youssef.abid@esprit.tn>
2019-02-12 21:44:51 +0100

    javaFx

---
+++ GestionAssuranceFx/src/gestionassurancefx/Services/Service_Reglement.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Services;
+
+import DataSource.Connexion;
+import Entities.Reglement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author youssef
+ */
+public class Service_Reglement {
+    Connection c= Connexion.getInstance().getConn();
+    public Service_Reglement(){}
+    public void ajouterReglement(Reglement R){
+       try{
+      PreparedStatement pt=c.prepareStatement("insert into reglement  values (?,?,?,?,?)");
+           pt.setInt(1,R.getCode_regl());
+           
+//l'indice par rapport a parametrede la requette du prepare
+           pt.setDate(2,R.getDate_regl());
+           pt.setInt(3,R.getFrais_expert());
+           pt.setInt(4,R.getMode_regl());
+           pt.setInt(5,R.getMontant_regl());
+           pt.executeUpdate();
+       }catch(SQLException ex){
+           System.out.println("Echec d ajouter le Reglement");
+       }
+             
+   }
+    public void afficherReglement(){
+		try { PreparedStatement pstmt=c.prepareStatement("select * from reglement");
+		            ResultSet rs=pstmt.executeQuery();
+       while(rs.next()){
+           System.out.println("Reglent : "+rs.getInt(1)+"code "+rs.getString(2)+"frais expert"+rs.getInt(3)+"mode"+rs.getInt(4)+"montant"+rs.getInt(5));
+       }
+		      
+       }
+       catch(SQLException ex){
+           System.out.println("echec affichage");
+       }
+       
+   }
+     
+        public void modifierReglement(Reglement R){
+       try{
+           PreparedStatement pt=c.prepareStatement("update reglement set code_regl=?,date_regl=?,frais_expert=?,mode_regl=?,montant_regl=? where code_regl=?");
+              pt.setInt(1,R.getCode_regl());
+           
+//l'indice par rapport a parametrede la requette du prepare
+           pt.setDate(2,R.getDate_regl());
+           pt.setInt(3,R.getFrais_expert());
+           pt.setInt(4,R.getMode_regl());
+           pt.setInt(5,R.getMontant_regl());
+           pt.setInt(6,R.getCode_regl());
+           pt.executeUpdate();       
+       }catch(SQLException ex){     
+       }
+   }
+        public void supprimerReglement(Reglement R){
+       try{
+           PreparedStatement pt=c.prepareStatement("delete from reglement where code_regl=?");
+              pt.setInt(1,R.getCode_regl());
+//l'indice par rapport a parametrede la requette du prepare
+           pt.executeUpdate();       
+       }catch(SQLException ex){     
+       }
+   }
+}
---
+++ GestionAssuranceFx/src/gestionassurancefx/Services/Service_Sinistre.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Services;
+
+import DataSource.Connexion;
+import Entities.Reglement;
+import Entities.Sinistre;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author youssef
+ */
+public class Service_Sinistre {
+    
+    Connection c= Connexion.getInstance().getConn();
+    public Service_Sinistre(){}
+    public void ajouterReglement(Sinistre R){
+       try{
+      PreparedStatement pt=c.prepareStatement("insert into sinistre  values (?,?,?,?,?,?,?,?,?)");
+           pt.setInt(1,R.getCode_sinistre());
+           
+//l'indice par rapport a parametrede la requette du prepare
+           pt.setDate(2,R.getDate_declaration());
+           pt.setDate(3,R.getDate_sinistre());
+           pt.setString(4,R.getLieu_sinistre());
+           pt.setInt(5,R.getNumero_sinistre());
+           pt.setInt(6,R.getDomage_mat());
+           pt.setInt(7,R.getDomage_corps());
+           pt.setInt(8,R.getCode_assurance());
+           pt.setString(9,R.getDescription());
+           pt.executeUpdate();
+       }catch(SQLException ex){
+           System.out.println("Echec d ajouter le Reglement");
+       }
+             
+   }
+    public void afficherSinistre(){
+		try { PreparedStatement pstmt=c.prepareStatement("select * from sinistre");
+		            ResultSet rs=pstmt.executeQuery();
+       while(rs.next()){
+           System.out.println("Sinistre : "+rs.getInt(1)+"date declaration"+rs.getDate(2)+"date sinistre "+rs.getDate(3)+"lieu sinistre"+rs.getString(4)+"numero sinistre"+rs.getInt(5)+"domage mat"+rs.getInt(6)+"domage corp"+rs.getInt(7)+"code assureur"+rs.getInt(8)+"Description "+rs.getString(9));
+       }
+		      
+       }
+       catch(SQLException ex){
+           System.out.println("echec affichage");
+       }
+       
+   }
+     
+        public void modifierSinistre(Sinistre R){
+       try{
+           PreparedStatement pt=c.prepareStatement("update sinistre set code_sinistre=?,date_declaration=?,date_sinistre=?,lieu_sinistre=?,numero_sinistre=?,dommage_mat=?,dommage_corp=?,code_assureur=?,description=? where code_sinistre=?");
+       pt.setInt(1,R.getCode_sinistre());
+           
+//l'indice par rapport a parametrede la requette du prepare
+           pt.setDate(2,R.getDate_declaration());
+           pt.setDate(3,R.getDate_sinistre());
+           pt.setString(4,R.getLieu_sinistre());
+           pt.setInt(5,R.getNumero_sinistre());
+           pt.setInt(6,R.getDomage_mat());
+           pt.setInt(7,R.getDomage_corps());
+           pt.setInt(8,R.getCode_assurance());
+           pt.setString(9,R.getDescription());
+           pt.setInt(10, R.getCode_sinistre());
+           pt.executeUpdate();       
+       }catch(SQLException ex){     
+       }
+   }
+        public void supprimerReglement(Sinistre R){
+       try{
+           PreparedStatement pt=c.prepareStatement("delete from sinistre where code_sinistre=?");
+              pt.setInt(1,R.getCode_sinistre());
+//l'indice par rapport a parametrede la requette du prepare
+           pt.executeUpdate();       
+       }catch(SQLException ex){     
+       }
+   }
+    
+}
