70f9479515329187b4e034ed4ac3342503f00e5a
ahmedderbel <ahmed.derbel@esprit.tn>
2019-02-24 19:58:59 +0100

    javaFX

--- GestionAssuranceFx/src/gestionassurancefx/Controllers/DashBController.java
+++ GestionAssuranceFx/src/gestionassurancefx/Controllers/DashBController.java
@@ -5,22 +5,30 @@
  */
 
 package gestionassurancefx.Controllers;
+
+import static gestionassurancefx.Controllers.LoginController.dashBStage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
@@ -38,10 +46,6 @@ public class DashBController  implements Initializable  {
     @FXML
     private AnchorPane mainView;
     @FXML
-    private Label projectBtn;
-    @FXML
-    private Button btnGConstat;
-    @FXML
     private Button btnGContrat;
     @FXML
     private Button btnGOffre;
@@ -51,48 +55,36 @@ public class DashBController  implements Initializable  {
     private Button btnGExpert;
     @FXML
     private Button btnGReparateur;
-	
+    @FXML
+    private Button btnGestonstat;
+    @FXML
     private AnchorPane mainAnchor;
-AnchorPane contrat;
-AnchorPane expert,reparateur;
-AnchorPane offre,reclamation;
-
+AnchorPane contrat,user,login,AssurePart,ajoutcontrat,constat;
+    @FXML
+    private Label projectBtn;
+    @FXML
+    private Button btnClientPart;
+    @FXML
+    private Label logoutLabel;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            contrat = FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/GestionAssureParticulier.fxml"));
-            expert=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/ExpertFXML.fxml"));
-            reparateur=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/ReparateurFXML.fxml"));
-            offre= FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/GestionOffre.fxml"));
-            reclamation=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/GestionReclamation.fxml"));
+            contrat = FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/ContratFXML.fxml"));
+            ajoutcontrat=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/ContratFXML.fxml"));
+            user=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/GestionUser1.fxml"));
+            login=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/Login.fxml"));
+            AssurePart=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/GestionAssurePartEntr.fxml"));
+            constat=FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/sinistre.fxml"));
         } catch (IOException ex) {
             Logger.getLogger(DashBController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }   
-    @FXML
-    private void constatView(ActionEvent event) throws IOException{
-       AnchorPane pane = FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/sinistre.fxml"));
-       // System.out.println(getClass().getResource("/gestionassurancefx/Views/GestionConstat.fxml"));
-        mainView.getChildren().setAll(pane);
-    }
-
-    @FXML
-    private void afficheSinistre(MouseEvent event) throws IOException {
-          AnchorPane pane = FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/sin.fxml"));
-        System.out.println(getClass().getResource("/gestionassurancefx/Views/sin.fxml"));
-       mainView.getChildren().setAll(pane);
-    }
-	
-	
-    private void btnConstatClicked(ActionEvent event) throws IOException{
-   
-         setNode(contrat);
-             }
-
-      private void setNode(Node node) {
+    
+       private void setNode(Node node) {
         mainView.getChildren().clear();
         mainView.getChildren().add((Node) node);
 
@@ -104,33 +96,69 @@ AnchorPane offre,reclamation;
         ft.setAutoReverse(false);
         ft.play();
     }
+    
+    
+    @FXML
+    private void constatView(ActionEvent event) throws IOException{
+        setNode(constat);
+    }
+    
+     @FXML
+    private void btnConstatClicked(ActionEvent event) throws IOException{
+         
+         setNode(contrat);
+             }
+
+    public void closeDashB() {
+        dashBStage = (Stage) dashBStage.getScene().getWindow();
+        dashBStage.close();
+    }
+    
+    
+   
+      
+      private void setLoginNode(Node node) {
+          
+        mainAnchor.getChildren().clear();
+        mainAnchor.getChildren().add((Node) node);
+        FadeTransition ft = new FadeTransition(Duration.millis(1500));
+        ft.setNode(node);
+        ft.setFromValue(0.1);
+        ft.setToValue(1);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
      
+    @FXML
     private void dashBoardClicked(MouseEvent event) throws IOException {
-            AnchorPane pane =FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/DashB.fxml"));
+           AnchorPane pane =FXMLLoader.load(getClass().getResource("/gestionassurancefx/Views/DashB.fxml"));
        mainAnchor.getChildren().setAll(pane);
+        	  
     }
 
     @FXML
-    private void btnExpertClicked(ActionEvent event) {
-                setNode(expert);
-
+    private void AgentClicked(MouseEvent event) {
+        setNode(user);
+        
     }
 
     @FXML
-    private void btnReparteurClicked(ActionEvent event) {
-                setNode(reparateur);
-
-    }
-	
-    private void btnOffreClicked(ActionEvent event) {
-            setNode(offre);
-            System.out.println("testtt");
+    private void btnClientPartClicked(ActionEvent event) {
+        setNode(AssurePart);
+        
     }
 
-    private void btnReclamationClicked(ActionEvent event) {
-            setNode(reclamation);
+    @FXML
+    private void LogoutClicked(MouseEvent event) throws IOException {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gestionassurancefx/Views/Login.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.setScene(new Scene(root1));
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+       stage.show();
+       closeDashB();
+    }
     
-    
-    
-}
 }
